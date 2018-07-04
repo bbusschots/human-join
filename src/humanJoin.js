a9421e4697c942b7ad8829de8182f4bd42180f21
Bart Busschots <opensource@bartificer.net>
2018-07-04 02:00:06 +0100

    Started Major Refactoring
    Change my mind completely on how the module will work. Effectively started over.

--- src/humanJoin.js
+++ src/humanJoin.js
@@ -1,50 +1,140 @@
 const is = require('is_js');
+const _ = require('lodash');
 
-const humanJoin = {
+/**
+ * The defined pre-processors.
+ *
+ * @protected
+ * @type {Map<string, function>}
+ */
+const PRE_PROCS = {};
+
+/**
+ * The defined renderers.
+ *
+ * @protected
+ * @type {Map<string, function>}
+ */
+const RENDERERS = {};
+
+/**
+ * The defined post-processors.
+ *
+ * @protected
+ * @type {Map<string, function>}
+ */
+const POST_PROCS = {};
+
+/**
+ * The joiner class.
+ */
+class Joiner{
     /**
-     * Join the elements of a list on a single line.
-     *
-     * The function is designed to render an array of primitive values. If
-     * anything else is passed the returned value will be the result of
-     * calling JavaScript's `String()` function on the value. The same
-     * goes for non-primitive values within arrays.
-     *
-     * @param {Array|object} data - the data to join.
-     * @param {object} [opts={}] - options.
-     * @param {string} [opts.separator=', '] - the separator to use between list elemnts.
-     * @param {string} [opts.conjunction=' & '] - the conjunction to use before the last item in the list.
-     * @return {string}
+     * @param {Object} [joinConfig={}]
      */
-    inline: function(data, opts){
-        if(is.not.array(data)) return String(data);
+    constructor(joinConfig = {}){
+        if(is.not.object(joinConfig)) joinConfig = {};
+        
+        /**
+         * @type {Object}
+         */
+        this._conf = _.assign({}, joinConfig);
+    }
+    
+    /**
+     * @param {string} name
+     * @param {function} fn
+     * @throws TypeRrror
+     * @throws {RangeError}
+     */
+    static registerPreProcessor(name, fn){
+        if(is.not.string(name) || is.empty(name)) throw new TypeError('name must be a single-line string of one or more letters, digits, or underscores');
+        if(is.not.function(fn)) throw new TypeError('preprocessor must be a callback');
+        if(is.not.undefined(PRE_PROCS[name])) throw new RangeError(`duplicate pre-processor '${name}'`);
+        PRE_PROCS[name] = fn;
+    }
+    
+    /**
+     * @param {string} name
+     * @param {function} fn
+     * @throws TypeRrror
+     * @throws {RangeError}
+     */
+    static registerRenderer(name, fn){
+        if(is.not.string(name) || is.empty(name)) throw new TypeError('name must be a single-line string of one or more letters, digits, or underscores');
+        if(is.not.function(fn)) throw new TypeError('renderer must be a callback');
+        if(is.not.undefined(RENDERERS[name])) throw new RangeError(`duplicate renderer '${name}'`);
+        RENDERERS[name] = fn;
+    }
+    
+    /**
+     * @param {string} name
+     * @param {function} fn
+     * @throws TypeRrror
+     * @throws {RangeError}
+     */
+    static registerPostProcessor(name, fn){
+        if(is.not.string(name) || is.empty(name)) throw new TypeError('name must be a single-line string of one or more letters, digits, or underscores');
+        if(is.not.function(fn)) throw new TypeError('preprocessor must be a callback');
+        if(is.not.undefined(POST_PROCS[name])) throw new RangeError(`duplicate post-processor '${name}'`);
+        POST_PROCS[name] = fn;
+    }
+    
+    /**
+     * @param {Array} data
+     * @param {Object} [opts={}]
+     * @returns {string}
+     */
+    join(data, opts){
         if(is.not.object(opts)) opts = {};
+        const conf = _.defaultsDeep({}, opts, this._conf);
         
-        // get a list of list parts
-        let parts = [];
-        if(is.array(data)){
-            parts = [...data];
-        }else{
-            for(const k of Object.keys(data)){
-                parts.push(`${k}=${String(data[k])}`);
-            }
-        }
+        // apply pre-processors to the data
+        // TO DO
         
-        // if the list is empty, return an empty string
-        if(parts.length === 0) return '';
+        // render the data
+        let ans = RENDERERS[conf.renderer.name](data, conf.renderer.opts);
         
-        // if there is exactly one element, return it
-        if(parts.length === 1) return String(parts[0]);
+        // apply post-processors
+        // TO DO
         
-        // there are multiple parts, so join them and return
-        if(is.not.string(opts.separator)) opts.separator = ', ';
-        if(is.not.string(opts.conjunction)) opts.conjunction = ' & ';
-        let ans = String(parts[0]);
-        for(let i = 1; i < parts.length; i++){
-            ans += `${i + 1 === parts.length ? opts.conjunction : opts.separator}${parts[i]}`;
-        }
+        // return the final result
         return ans;
     }
-};
-//humanJoin.amp = humanJoin.ampersand;
+}
 
-module.exports = humanJoin;
+Joiner.registerRenderer('inline', function(data, opts){
+    if(is.not.array(data)) return String(data);
+    if(is.not.object(opts)) opts = {};
+    
+    // get a list of list parts
+    let parts = [];
+    if(is.array(data)){
+        parts = [...data];
+    }else{
+        for(const k of Object.keys(data)){
+            parts.push(`${k}=${String(data[k])}`);
+        }
+    }
+    
+    // if the list is empty, return an empty string
+    if(parts.length === 0) return '';
+    
+    // if there is exactly one element, return it
+    if(parts.length === 1) return String(parts[0]);
+    
+    // there are multiple parts, so join them and return
+    if(is.not.string(opts.separator)) opts.separator = ', ';
+    if(is.not.string(opts.conjunction)) opts.conjunction = ' & ';
+    let ans = String(parts[0]);
+    for(let i = 1; i < parts.length; i++){
+        ans += `${i + 1 === parts.length ? opts.conjunction : opts.separator}${parts[i]}`;
+    }
+    return ans;
+});
+
+module.exports = new Joiner({
+    renderer: { name: 'inline', opts: {} },
+    preProcessors: {},
+    postProcessors: {}
+});
