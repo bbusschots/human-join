b126d19d27256cafa7ea7235e64f225a12d07c15
Bart Busschots <opensource@bartificer.net>
2018-07-05 01:56:34 +0100

    Made a start on the quote Pre-Processor
    Made a start on adding in a pre-processor to quote the data before including it in the output.

--- src/humanJoin.js
+++ src/humanJoin.js
@@ -96,7 +96,10 @@ class Joiner{
         const conf = _.defaultsDeep({}, opts, this._conf);
         
         // apply pre-processors to the data
-        // TO DO
+        for(const pp of opts.preProcessors){
+            // TO DO - add more tests and try/catch
+            PRE_PROCS[pp.name](data, pp.opts);
+        }
         
         // render the data
         let ans = RENDERERS[conf.renderer.name](data, conf.renderer.opts);
@@ -137,6 +140,44 @@ Joiner.registerRenderer('inline', function(data, opts){
         ans += `${i + 1 === parts.length ? opts.conjunction : opts.separator}${parts[i]}`;
     }
     return ans;
+});
+
+const CHAR_MIRROR_MAP = {
+    '!' : '¡',
+    '?' : '¿',
+    '(' : ')',
+    ')' : '(',
+    '{' : '}',
+    '}' : '{',
+    '[' : ']',
+    ']' : '[',
+    '<' : '>',
+    '>' : '<'
+};
+function mirrorCharacter(c){
+    if(is.not.string(c) && is.not.number(c)) return '';
+    c = String(c); // force to string
+    if(is.empty(c)) return '';
+    if(c.length > 1) c = c.substring(0, 1); // truncate to a single character
+    return is.string(CHAR_MIRROR_MAP[c]) ? CHAR_MIRROR_MAP[c] : c;
+}
+function mirrorString(s){
+    if(is.not.string(s) && is.not.number(s)) return '';
+    s = String(s); // force to string
+    let ans = '';
+    for(const c of s.split('').reverse()){
+        ans += mirrorCharacter(c);
+    }
+    return ans;
+}
+Joiner.registerPreProcessor('quote', function(data, opts){
+    if(is.not.array(data)) return;
+    if(is.not.object(opts)) opts = {};
+    if(is.not.string(opts.quoteWith) || is.empty(opts.quoteWith)) opts.quoteWith = "'";
+    opts.mirror = opts.mirror ? true : false;
+    for(let i = 0; i < data.length; i++){
+        data[i] = `${opts.quoteWith}${data[i]}${opts.mirror ? mirrorString(opts.quoteWith) : opts.quoteWith}`;
+    }
 });
 
 module.exports = new Joiner({
