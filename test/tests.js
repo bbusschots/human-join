2e31facf4eb1dfe721912982959be3b8bd982ec0
Bart Busschots <opensource@bartificer.net>
2018-08-15 00:57:16 +0100

    Continued work on tests

--- test/tests.js
+++ test/tests.js
@@ -3,20 +3,27 @@ QUnit.module('inline renderer', function(){
         a.expect(11);
         
         // make sure the function exists
-        a.ok(is.function(humanJoin.inline), 'humanJoin.inline is a function');
+        a.ok(is.function(humanJoin.join), 'humanJoin.join is a function');
         
         // Check that various types of data return the expected values
-        a.strictEqual(humanJoin.inline(), String(undefined), 'undefined as data handled correctly');
-        a.strictEqual(humanJoin.inline({}), String({}), 'plain object as data handled correctly');
+        a.strictEqual(humanJoin.join(), String(undefined), 'undefined as data handled correctly');
+        a.strictEqual(humanJoin.join({}), String({}), 'plain object as data handled correctly');
         let obj = new Date();
-        a.strictEqual(humanJoin.inline(obj), String(obj), 'prototyped object as data handled correctly');
-        a.strictEqual(humanJoin.inline('boogers'), 'boogers', 'string as data handled correctly');
-        a.strictEqual(humanJoin.inline(42), '42', 'number as data handled correctly');
+        a.strictEqual(humanJoin.join(obj), String(obj), 'prototyped object as data handled correctly');
+        a.strictEqual(humanJoin.join('boogers'), 'boogers', 'string as data handled correctly');
+        a.strictEqual(humanJoin.join(42), '42', 'number as data handled correctly');
         let fn = function(){};
-        a.strictEqual(humanJoin.inline(fn), String(_.cloneDeep(fn)), 'function reference as data handled correctly');
-        a.strictEqual(humanJoin.inline([]), '', 'empty array handled correctly');
-        a.strictEqual(humanJoin.inline(['boogers']), 'boogers', 'single element array handled correctly');
-        a.strictEqual(humanJoin.inline(['boogers', 'snot']), 'boogers & snot', 'two element array handled correctly');
-        a.strictEqual(humanJoin.inline(['boogers', 'snot', 'bogies']), 'boogers, snot & bogies', 'three element array handled correctly');
+        a.strictEqual(humanJoin.join(fn), String(_.cloneDeep(fn)), 'function reference as data handled correctly');
+        a.strictEqual(humanJoin.join([]), '', 'empty array handled correctly');
+        a.strictEqual(humanJoin.join(['boogers']), 'boogers', 'single element array handled correctly');
+        a.strictEqual(humanJoin.join(['boogers', 'snot']), 'boogers & snot', 'two element array handled correctly');
+        a.strictEqual(humanJoin.join(['boogers', 'snot', 'bogies']), 'boogers, snot & bogies', 'three element array handled correctly');
+    });
+    
+    QUnit.test('.j() shortcut', function(a){
+        a.expect(3);
+        a.ok(is.function(humanJoin.j), 'shortcut function exists');
+        a.strictEqual(humanJoin.j, humanJoin.join, 'maps to expected function');
+        a.strictEqual(humanJoin.j(['boogers', 'snot', 'bogies']), 'boogers, snot & bogies', 'produces expected output');
     });
 });
